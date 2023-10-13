86bdf7ff18802dbd6c47dc81d1d8838c0471f9fc
Eka <eka.abashidze@gmail.com>
2023-10-13 14:46:40 +0400

    feat: add background images for mobile and desktop t body

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        // "bg-mobile": "url('../public/images/bg-sidebar-mobile.svg')",
+      },
+    },
   },
   plugins: [],
 };
